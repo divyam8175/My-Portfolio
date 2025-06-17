850c2a6fa81e8c572d3d4c74e2d1d8d4a5fcdd28
DIVYAM VERMA <divyamverma90@gmail.com>
2025-06-18 01:06:49 +0530

    Add new skill images and update Study component with enhanced styling and data structure

--- src/components/Study.jsx
+++ src/components/Study.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-// import './education.css'; // Assuming you have a CSS file for styling
+import '../css/study.css'; // Assuming you have a CSS file for styling
 import education from "./data/education.json"; // Adjust the path as necessary
 
 const Study= () => {
     return (
         <>
-        <div className="container skill">
+        <div className="container1">
             <h1>EDUCATION</h1>
-            <div className="items">
+            <div className="container2">
                 {
                     education.map((data) => (
                         <>
-                        <div>
-                            <h1>{data.Course}</h1>
-                            <p>{data.institution}</p>
-                            <p>{data.duration}</p>
-                            <p>{data.Percentage}</p>
+                        <div className="container3"
+                key={data.id} data-aos="flip-left"
+                data-aos-duration="1000">
+                            <h2><b>{data.Course}</b></h2>
+                            <h3>{data.stream}</h3>
+                            <h4>{data.institution}</h4>
+                            <h5>{data.duration}</h5>
+                            <h6>{data.Percentage}</h6>
                         </div>
                         </>
                     ))}
