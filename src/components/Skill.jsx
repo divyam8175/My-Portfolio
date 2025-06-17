3a0dda9e0a1ae4c9dc8439ce12470f75d7895353
DIVYAM VERMA <divyamverma90@gmail.com>
2025-06-17 23:49:46 +0530

    Refactor education component for improved structur e and readability

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import Skill from "./data/skills.json";
- const Skills = () => {
+const Skills = () => {
   return (
     <>
-    <div className="container skill" id="skills">
+      <div className="container skill" id="skills">
         <h1>SKILLS</h1>
         <div className="items">
-        {Skill.map((data) => (
-                <>
-                <div className="item" key={data.id}
+          {Skill.map((data) => (
+            <>
+              <div
+                className="item"
+                key={data.id}
                 data-aos="flip-left"
                 data-aos-duration="1000"
-                >
-                        <img src={`/assets/${data.imageSrc}`} alt="" />
-                        <h3>{data.title}</h3>
-                </div>
-                </>
-            ))}
+              >
+                <img src={`/assets/${data.imageSrc}`} alt="" />
+                <h3>{data.title}</h3>
+              </div>
+            </>
+          ))}
         </div>
-    </div>
+      </div>
     </>
   );
 };
 
-export default Skills
+export default Skills;
--- src/components/education.jsx
+++ src/components/education.jsx
@@ -1,9 +1,20 @@
-
+import React from 'react';
+import './education.css'; // Assuming you have a CSS file for styling
+import education from "./data/education.json"; // Adjust the path as necessary
 
 const education= () => {
     return (
         <>
-        hello;
+        <div className="container education">
+            <h1>EDUCATION</h1>
+            <div className="items">
+                {
+                    education.map((data) => (
+                        <>
+                        </>
+                    ))};
+            </div> 
+        </div>
         </>
     );
 };
